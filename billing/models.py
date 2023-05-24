8ccd2c49b7f959209a66def0520d36f9b8db48f0
Usaid Aijaz <malikusaid10@gmail.com>
2023-05-24 18:20:15 +0530

    Fix ForeignKey Errors

--- billing/models.py
+++ billing/models.py
@@ -13,14 +13,16 @@ class User(Base):
 class Meter(Base):
     __tablename__ = "meters"
 
-    meter_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    meter_id = Column(Integer, primary_key=True,
+                      autoincrement=True, index=True)
     initial_reading = Column(Integer, nullable=False)
 
 
 class QuarterType(Base):
     __tablename__ = "quarter_types"
 
-    quarter_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    quarter_id = Column(Integer, primary_key=True,
+                        autoincrement=True, index=True)
     quarter_name = Column(String, nullable=False)
 
     rooms = relationship("Room", back_populates="quarter_type")
@@ -30,7 +32,8 @@ class Room(Base):
     __tablename__ = "rooms"
 
     room_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    quarter_type_id = Column(Integer, ForeignKey("QuarterType.quarter_id"), nullable=False)
+    quarter_type_id = Column(Integer, ForeignKey(
+        "quarter_types.quarter_id"), nullable=False)
     room_number = Column(Integer, nullable=False)
     is_metered = Column(Boolean, nullable=False)
 
@@ -40,9 +43,10 @@ class Room(Base):
 class UserToRoom(Base):
     __tablename__ = "user_to_room"
 
-    user_to_room_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    user_id = Column(Integer, ForeignKey("User.user_id"), nullable=False)
-    room_id = Column(Integer, ForeignKey("Room.room_id"), nullable=False)
+    user_to_room_id = Column(Integer, primary_key=True,
+                             autoincrement=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id"), nullable=False)
 
     # user = relationship("User", back_populates="user_to_room")
     # room = relationship("Room", back_populates="user_to_room"
@@ -51,9 +55,10 @@ class UserToRoom(Base):
 class MeterToRoom(Base):
     __tablename__ = "meter_to_room"
 
-    meter_to_room_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    meter_id = Column(Integer, ForeignKey("Meter.meter_id"), nullable=False)
-    room_id = Column(Integer, ForeignKey("Room.room_id"), nullable=False)
+    meter_to_room_id = Column(
+        Integer, primary_key=True, autoincrement=True, index=True)
+    meter_id = Column(Integer, ForeignKey("meters.meter_id"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id"), nullable=False)
 
     # meter = relationship("Meter", back_populates="meter_to_room")
     # room = relationship("Room", back_populates="meter_to_room")
@@ -62,7 +67,8 @@ class MeterToRoom(Base):
 class FlatRate(Base):
     __tablename__ = "flat_rates"
 
-    flat_rate_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    flat_rate_id = Column(Integer, primary_key=True,
+                          autoincrement=True, index=True)
     flat_rate_name = Column(String, nullable=False)
     flat_rate_value = Column(Integer, nullable=False)
 
@@ -70,9 +76,11 @@ class FlatRate(Base):
 class FlatRateToRoom(Base):
     __tablename__ = "flat_rate_to_room"
 
-    flat_rate_to_room_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    flat_rate_id = Column(Integer, ForeignKey("FlatRate.flat_rate_id"), nullable=False)
-    room_id = Column(Integer, ForeignKey("Room.room_id"), nullable=False)
+    flat_rate_to_room_id = Column(
+        Integer, primary_key=True, autoincrement=True, index=True)
+    flat_rate_id = Column(Integer, ForeignKey(
+        "flat_rates.flat_rate_id"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id"), nullable=False)
 
     # flat_rate = relationship("FlatRate", back_populates="flat_rate_to_room")
     # room = relationship("Room", back_populates="flat_rate_to_room")
@@ -81,7 +89,8 @@ class FlatRateToRoom(Base):
 class MeterRate(Base):
     __tablename__ = "meter_rates"
 
-    meter_rate_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    meter_rate_id = Column(Integer, primary_key=True,
+                           autoincrement=True, index=True)
     meter_rate_name = Column(String, nullable=False)
     meter_rate_upto = Column(Integer, nullable=False)
     meter_rate_value = Column(Integer, nullable=False)
@@ -90,8 +99,9 @@ class MeterRate(Base):
 class Reading(Base):
     __tablename__ = "readings"
 
-    reading_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    meter_id = Column(Integer, ForeignKey("Meter.meter_id"), nullable=False)
+    reading_id = Column(Integer, primary_key=True,
+                        autoincrement=True, index=True)
+    meter_id = Column(Integer, ForeignKey("meters.meter_id"), nullable=False)
     month = Column(Integer, nullable=False)
     year = Column(Integer, nullable=False)
     units_consumed = Column(Integer, nullable=False)
@@ -104,9 +114,9 @@ class Bill(Base):
     __tablename__ = "bills"
 
     bill_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    user_id = Column(Integer, ForeignKey("User.user_id"), nullable=False)
-    room_id = Column(Integer, ForeignKey("Room.room_id"), nullable=False)
-    meter_id = Column(Integer, ForeignKey("Meter.meter_id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id"), nullable=False)
+    meter_id = Column(Integer, ForeignKey("meters.meter_id"), nullable=False)
     month = Column(Integer, nullable=False)
     year = Column(Integer, nullable=False)
     amount = Column(Integer, nullable=False)
